d429a39b75c9d859fd4115a5c54011c444d6623f
blagisquet <baptiste.lagisquet@gmail.com>
2019-11-26 13:35:24 +0100

    navbar workin

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,32 +1,49 @@
 import React from 'react';
+import Home from '../Home/Home';
+import Qi from '../Qi/Qi';
+import Sports from '../Sports/Sports';
+import {
+  Link, BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
 function Menu() {
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-        </a>
-
-        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-          <a href="/" className="navbar-item">
-            Home
+    <div>
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
           </a>
-          <a href="/sports" className="navbar-item">
-            Sports
-            </a>
-          <a href="/qi" className="navbar-item">
-            General knowledge
+
+          <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
           </a>
         </div>
-      </div>
-    </nav>
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-start is-centered">
+            <Router>
+              <Link to="/" className="navbar-item">Home</Link>
+              <Link to="/sports" className="navbar-item">Sports</Link>
+              <Link to="/qi" className="navbar-item">Qi</Link>
+              <Switch>
+                <Route path="/">
+                  <Home />
+                </Route>
+                <Route path="/sports">
+                  <Sports />
+                </Route>
+                <Route path="/qi">
+                  <Qi />
+                </Route>
+              </Switch>
+            </Router>
+          </div>
+        </div>
+      </nav>
+    </div>
   )
 }
 
